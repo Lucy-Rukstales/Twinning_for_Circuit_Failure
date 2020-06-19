9b4beb658a8aeb8949414799b26eff4f4ba05c49
Lucy Rukstales <43323790+Lucy-Rukstales@users.noreply.github.com>
2020-06-19 16:19:41 -0400

    Add files via upload

---
+++ ADC_Control.v
@@ -0,0 +1,43 @@
+//////////////////////////////////////////////////////
+// Date: 				6/19/2020
+// Contributors: 		Lucy Rukstales, Michaela Mitchell
+//
+// Description: 		This file allows for data collection from an analog to digital converter (ADC)
+//							This trial runs the ADC in PIC Mode, using a 50kHz clock
+//
+// Components Used:	MIKROE-340
+//////////////////////////////////////////////////////
+
+module ADC_Control(CS,P4,NC,P3,P5,clk);
+
+	input P4;  // MISO
+	input clk; // 50MHz FPGA clock
+	
+	output CS,NC;
+	
+	output reg P3; // 50kHz ADC clock
+	output reg P5; // MOSI
+	
+	reg [9:0]counter;
+	
+	//----------------------------------------------------
+	// Scale the clk from 50MHz to 50kHz
+	// P3 to be used with the ADC
+	always @ (posedge clk) begin
+		
+		if(counter < 10'd500) begin
+			P3 <= 1'b1;
+			counter <= counter + 1'b1;
+		end
+		
+		else if (counter < 10'd999) begin
+			P3 <= 1'b0;
+			counter <= counter + 1'b1;
+		end
+		
+		else
+			counter <= 1'b0;
+			
+	end
+	
+endmodule
