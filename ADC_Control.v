bf576508593a61ac363aeff8fe32738fa77da7a4
Lucy Rukstales <43323790+Lucy-Rukstales@users.noreply.github.com>
2020-06-19 18:32:59 -0400

    Update ADC_Control.v
    initial attempt at creating ADC MISO to read and store 12-bits

--- ADC_Control.v
+++ ADC_Control.v
@@ -8,17 +8,18 @@
 // Components Used:	MIKROE-340
 //////////////////////////////////////////////////////
 
-module ADC_Control(clk,P3);//CS,P4,NC,P3,P5,clk);
+module ADC_Control(clk,P3,CS,P4);//,NC,P3,P5,clk);
 
-	//input P4;  // MISO
+	input P4;  // MISO
 	input clk; // 50MHz FPGA clock
 	
-	//output CS,NC;
-	
+	output reg CS;//,NC;
 	output reg P3; // 50kHz ADC clock
 	//output reg P5; // MOSI
 	
 	reg [9:0]counter;
+	reg [4:0]cnt13;
+	reg [11:0]sample;
 	
 	//----------------------------------------------------
 	// Scale the clk from 50MHz to 50kHz
@@ -38,6 +39,36 @@ module ADC_Control(clk,P3);//CS,P4,NC,P3,P5,clk);
 		else
 			counter <= 1'b0;
 			
+	end
+	
+	//----------------------------------------------------
+	// Count to 13 using cnt13
+	always @(posedge P3) begin
+	
+		if (cnt13 == 4'd12)
+			cnt13 <= 1'b0;
+			
+		cnt13 <= cnt13 + 1'b1;
+		
+	end
+	
+	//----------------------------------------------------
+	// Read from the ADC, 12-bits at a time
+	// P4 to be used for MISO
+	// This may end up a bit off with each iteration due to CS
+	always @ (posedge P3) begin
+	
+		if (cnt13 == 1'b0)
+			CS <= 1'b0;
+		
+		else if (cnt13 <= 4'd12) begin
+			sample[11:1] <= sample[10:0];
+			sample[0] <= P4;
+		end
+			
+		else
+			CS <= 1'b1;
+		
 	end
 	
 endmodule
