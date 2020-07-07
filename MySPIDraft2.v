51d5a73439a795fdade979ea0d06318295698f84
Lucy Rukstales <43323790+Lucy-Rukstales@users.noreply.github.com>
2020-07-07 14:59:43 -0400

    Update MySPIDraft2.v

--- MySPIDraft2.v
+++ MySPIDraft2.v
@@ -2,7 +2,7 @@
 // Transmit 1 byte of defined data on 250kHz SCL
 //CPOL: 0 (SCL starts low) CPHA: 1 (Sample MOSI on falling edge SCL)
 
-module MySPI (clk, rst, SCL, SS, MOSI);
+module My_SPI (clk, rst, SCL, SS, MOSI);
 input clk; //On board 50MHz clk
 input rst; //Manual switch
 output reg SCL; //Clk line to sync data transfer
@@ -30,7 +30,7 @@ begin
 		if (countSCL == 26'd0) //implement a delay for MOSI to change
 		begin
 			SCLtracker <= SCLtracker + 26'd1; //SCL == +2 for each period
-			if (SCLtracker >= 26'd2 && SCLtracker <= 26'd17) //1 period delay for SS to start and MOSI to intialize
+			if (SCLtracker >= 26'd2 && SCLtracker <= 26'd35) //1 period delay for SS to start and MOSI to intialize
 				SCL <= ~SCL;
 			else
 				SCL <= 1'b0;
@@ -46,15 +46,15 @@ begin
 		SS <= 1'b1;
 	else
 	begin
-		if (SCLtracker == 26'd1) //1st SCL clock cycle
+		if (SCLtracker == 26'd1 || SCLtracker == 26'd19) //1st SCL clock cycle
 			SS <= 1'b0;
-		else if (SCLtracker == 26'd18) //After MOSI done transmitting
+		else if (SCLtracker == 26'd18 || SCLtracker == 26'd36) //After MOSI done transmitting
 			SS <= 1'b1;
 	end
 end
 
 //Define MOSI line
-reg[7:0] txData = 8'b10101011; //test data
+reg[11:0] txData = 12'b111100111000; //test data
 always @ (posedge clk or negedge rst)
 begin
 	if (rst == 1'b0)
@@ -64,22 +64,40 @@ begin
 		case(SCLtracker)
 			0: begin MOSI <= 1'b0; end //Waiting for SS to go low
 			1: begin MOSI <= 1'b0; end //Waiting for SS to go low
-			2: begin MOSI <= txData[7]; end
-			3: begin MOSI <= txData[7]; end
-			4: begin MOSI <= txData[6]; end
-			5: begin MOSI <= txData[6]; end
-			6: begin MOSI <= txData[5]; end
-			7: begin MOSI <= txData[5]; end
-			8: begin MOSI <= txData[4]; end
-			9: begin MOSI <= txData[4]; end
-			10: begin MOSI <= txData[3]; end
-			11: begin MOSI <= txData[3]; end
-			12: begin MOSI <= txData[2]; end
-			13: begin MOSI <= txData[2]; end
-			14: begin MOSI <= txData[1]; end
-			15: begin MOSI <= txData[1]; end
-			16: begin MOSI <= txData[0]; end
-			17: begin MOSI <= txData[0]; end
+			2: begin MOSI <= 1'b0; end
+			3: begin MOSI <= 1'b0; end
+			4: begin MOSI <= 1'b0; end
+			5: begin MOSI <= 1'b0; end
+			6: begin MOSI <= 1'b0; end
+			7: begin MOSI <= 1'b0; end
+			8: begin MOSI <= 1'b0; end
+			9: begin MOSI <= 1'b0; end
+			10: begin MOSI <= txData[11]; end
+			11: begin MOSI <= txData[11]; end
+			12: begin MOSI <= txData[10]; end
+			13: begin MOSI <= txData[10]; end
+			14: begin MOSI <= txData[9]; end
+			15: begin MOSI <= txData[9]; end
+			16: begin MOSI <= txData[8]; end
+			17: begin MOSI <= txData[8]; end
+			18: begin MOSI <= 1'b0; end //Waiting for SS to go low
+			19: begin MOSI <= 1'b0; end //Waiting for SS to go low
+			20: begin MOSI <= txData[7]; end
+			21: begin MOSI <= txData[7]; end
+			22: begin MOSI <= txData[6]; end
+			23: begin MOSI <= txData[6]; end
+			24: begin MOSI <= txData[5]; end
+			25: begin MOSI <= txData[5]; end
+			26: begin MOSI <= txData[4]; end
+			27: begin MOSI <= txData[4]; end
+			28: begin MOSI <= txData[3]; end
+			29: begin MOSI <= txData[3]; end
+			30: begin MOSI <= txData[2]; end
+			31: begin MOSI <= txData[2]; end
+			32: begin MOSI <= txData[1]; end
+			33: begin MOSI <= txData[1]; end
+			34: begin MOSI <= txData[0]; end
+			35: begin MOSI <= txData[0]; end
 			default: MOSI <= 1'b0;
 		endcase
 	end
