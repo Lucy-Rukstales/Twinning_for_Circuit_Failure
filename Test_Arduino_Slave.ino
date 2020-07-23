0251d9eac8ab6ed641d147241750532e5e915bf6
Lucy Rukstales <43323790+Lucy-Rukstales@users.noreply.github.com>
2020-07-23 09:47:40 -0400

    Update Test_Arduino_Slave.ino

--- Test_Arduino_Slave.ino
+++ Test_Arduino_Slave.ino
@@ -1,47 +1,55 @@
 // FPGA-Arduino SPI Protocol
-// Arduino Slave
+// Arduino Child
 
 //Notes:
 //Pin defintions (SCL, MOSI, SS) are inherently defined when SPI slave mode enabled (from datasheet)
 //SPCR is the "SPI Control Register"
-  /* SPCR, default all are zeros, set to ones in setup
-    | 7    | 6    | 5    | 4    | 3    | 2    | 1    | 0    |
-    | SPIE | SPE  | DORD | MSTR | CPOL | CPHA | SPR1 | SPR0 |
-    SPIE - Enables the SPI interrupt when 1
-    SPE - Enables the SPI when 1
-    DORD - Sends data least Significant Bit First when 1, most Significant Bit first when 0
-    MSTR - Sets the Arduino in master mode when 1, slave mode when 0
-    CPOL - Sets the data clock to be idle when high if set to 1, idle when low if set to 0
-    CPHA - Samples data on the falling edge of the data clock when 1, rising edge when 0
-    SPR1 and SPR0 - Sets the SPI speed, 00 is fastest (4MHz) 11 is slowest (250KHz) (master only) */
 
 #include<SPI.h> //SPI library
 volatile int i = 0; //volatile types to be sent to ISR
-byte myArray[2];
+byte twelveBitArray[2]; //temporary storage array from ISR (2 bytes for each 12 bit sample)
 
-void setup()
-{
+
+const int numberOfSamples = 1; //Change based on how much data user wishes to collect
+int sampleNumber = 0; //Intialize counter
+int storedData[numberOfSamples] = {0}; //Final storage array
+int x, y;
+
+void setup() {
   Serial.begin(9600);
   SPCR |= _BV(SPE); //Enable slave mode and SPI
   SPCR |= _BV(SPIE); //Attach interrupts
-  //SPCR |= _BV(CPHA); //Sample on falling edge of SCL (data mode 1)
 }
 
-void loop(void){
-  if (i >= 1){
-    int x = (int)myArray[0];
-    x = x<<8;
-    int y = (int)myArray[1];
-    int z = x|y;
-    Serial.print("Received data item from FPGA-Master: ");
-    Serial.println(z, BIN); //Binary for testing, likely hex later
-    i=0;
-    Serial.println("=============================================");
+void loop(void) {
+  if (i >= 1) {
+    
+    //Store new sample in storage array, print it out if finished
+    if (sampleNumber < numberOfSamples) {
+      x = (int)twelveBitArray[0];
+      x = x << 8;
+      delay(50);
+      y = (int)twelveBitArray[1];
+      noInterrupts();
+      storedData[sampleNumber] = x | y;
+      sampleNumber = sampleNumber + 1;
+      interrupts();
+    }else if (sampleNumber == numberOfSamples) {
+      noInterrupts();
+      Serial.print(sampleNumber);
+      Serial.println(" samples have been stored: ");
+      for (int j = 0; j < sampleNumber; j++) {
+        Serial.println(storedData[j],BIN);
+      }
+      interrupts();
+    }
+    
+    i = 0;
   }
 }
 
-ISR (SPI_STC_vect)   //Interrput service "routine SPI trasnfer complete vector"
+ISR (SPI_STC_vect)   //Interrput service "routine SPI transfer complete vector"
 {
-  myArray[i] = SPDR; //SPDR: SPI Data Register (Sampled data from MOSI)
+  twelveBitArray[i] = SPDR; //SPDR: SPI Data Register (Sampled data from MOSI)
   i++;
 }
