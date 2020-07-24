6b9d5055eb00b371d2c5890342d9a15a1442cc8d
Lucy Rukstales <43323790+Lucy-Rukstales@users.noreply.github.com>
2020-07-24 17:14:17 -0400

    Update Test_Arduino_Slave.ino

--- Test_Arduino_Slave.ino
+++ Test_Arduino_Slave.ino
@@ -7,18 +7,7 @@
 
 #include<SPI.h> //SPI library
 volatile int i = 0; //volatile types to be sent to ISR
-volatile byte twelveBitArray[2]; //temporary storage array from ISR (2 bytes for each 12 bit sample)
-
-// FPGA-Arduino SPI Protocol
-// Arduino Child
-
-//Notes:
-//Pin defintions (SCL, MOSI, SS) are inherently defined when SPI slave mode enabled (from datasheet)
-//SPCR is the "SPI Control Register"
-
-#include<SPI.h> //SPI library
-volatile int i = 0; //volatile types to be sent to ISR
-const int numberOfSamples = 2; //Change based on how much data user wishes to collect //2048
+const int numberOfSamples = 8; //Change based on how much data user wishes to collect //2048
 byte twelveBitArray[numberOfSamples*2]; //temporary storage array from ISR (2 bytes for each 12 bit sample)// 4096
 int sampleNumber = 0; //Intialize counter
 int sample[numberOfSamples]; // Final storage array
@@ -40,7 +29,7 @@ void loop(void) { // variable "processupto"
       }else {
         y = (int)twelveBitArray[j];
         sample[j/2] = x | y;
-      }Serial.println(twelveBitArray[j],BIN);
+      }//Serial.println(twelveBitArray[j],BIN);
     }
     delay(5000);
     for (int k=0; k < numberOfSamples; k++){
@@ -54,60 +43,6 @@ void loop(void) { // variable "processupto"
 }
 
 ISR (SPI_STC_vect)   //Interrput service "routine SPI transfer complete vector" // this is fine as is // turn of ISR when get to 4096
-{
-  twelveBitArray[i] = SPDR; //SPDR: SPI Data Register (Sampled data from MOSI)
-  i++;
-}
-const int numberOfSamples = 4; //Change based on how much data user wishes to collect
-int sampleNumber = 0; //Intialize counter
-int storedData[numberOfSamples] = {0}; //Final storage array
-
-void setup() {
-  Serial.begin(9600);
-  SPCR |= _BV(SPE); //Enable slave mode and SPI
-  SPCR |= _BV(SPIE); //Attach interrupts
-}
-
-void loop(void) {
-
-  if (i >= 1) {
-    //Store new sample in storage array, print it out if finished
-    int x = (int)twelveBitArray[0];
-    delay(10);
-    x = x << 8;
-    delay(10);
-    int y = (int)twelveBitArray[1];
-    delay(10);
-    noInterrupts();
-    int z = x | y;
-    
-    storedData[sampleNumber] = z;
-    sampleNumber++;  
-    if (sampleNumber == numberOfSamples) {
-      for (int j = 0; j < sampleNumber; j++) {
-        Serial.println(storedData[j], BIN);
-      }
-      sampleNumber++;
-    }
-    interrupts();
-
-    i = 0;
-  }
-
-  //  noInterrupts();
-  //  if (sampleNumber == numberOfSamples) {
-  //
-  //    Serial.print(sampleNumber);
-  //    Serial.println(" samples have been stored: ");
-  //    for (int j = 0; j < sampleNumber; j++) {
-  //      Serial.println(storedData[j], BIN);
-  //    }
-  //    sampleNumber++;
-  //  }
-  //  interrupts();
-}
-
-ISR (SPI_STC_vect)   //Interrput service "routine SPI transfer complete vector"
 {
   twelveBitArray[i] = SPDR; //SPDR: SPI Data Register (Sampled data from MOSI)
   i++;
