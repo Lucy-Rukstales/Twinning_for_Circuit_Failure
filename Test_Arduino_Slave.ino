511c2e56556715d06cf555fbf30598fab4bbadb0
Lucy Rukstales <43323790+Lucy-Rukstales@users.noreply.github.com>
2020-07-29 13:38:37 -0400

    Update Test_Arduino_Slave.ino

--- Test_Arduino_Slave.ino
+++ Test_Arduino_Slave.ino
@@ -7,35 +7,43 @@
 
 #include<SPI.h> //SPI library
 volatile int i = 0; //volatile types to be sent to ISR
-const int numberOfSamples = 8; //Change based on how much data user wishes to collect //2048
-byte twelveBitArray[numberOfSamples*2]; //temporary storage array from ISR (2 bytes for each 12 bit sample)// 4096
+const int numberOfSamples = 100; //Change based on how much data user wishes to collect //2048
+byte twelveBitArray[numberOfSamples * 2]; //temporary storage array from ISR (2 bytes for each 12 bit sample)// 4096
 int sampleNumber = 0; //Intialize counter
 int sample[numberOfSamples]; // Final storage array
-int x,y,z;
+int x, y, z;
 
 void setup() {
   Serial.begin(9600);
-  SPCR |= _BV(SPE); //Enable slave mode and SPI
-  SPCR |= _BV(SPIE); //Attach interrupts
+  SPCR |= bit(SPE);
+  //SPCR |= _BV(SPE); //Enable slave mode and SPI
+  //SPCR |= _BV(SPIE); //Attach interrupts
+
+  pinMode(SCL, INPUT);
+  pinMode(MOSI, INPUT);
+  pinMode(SS, INPUT);
+
+  SPI.attachInterrupt();
 }
 
 void loop(void) { // variable "processupto"
-  if (i == numberOfSamples*2){
+  if (i == numberOfSamples * 2) {
     noInterrupts();
-    for (int j=0; j < numberOfSamples*2; j++){
-      if (j%2 == 0){
+    for (int j = 0; j < numberOfSamples * 2; j++) {
+      if (j % 2 == 0) {
         x = (int)twelveBitArray[j];
-        x = x<<8;
-      }else {
+        x = x << 8;
+      } else {
         y = (int)twelveBitArray[j];
-        sample[j/2] = x | y;
+        sample[j / 2] = x | y;
       }//Serial.println(twelveBitArray[j],BIN);
     }
-    delay(5000);
-    for (int k=0; k < numberOfSamples; k++){
-      Serial.print(k);
-      Serial.print(": ");
-      Serial.println(sample[k],BIN);
+    for (int k = 0; k < numberOfSamples; k++) {
+//      Serial.print(k);
+//      Serial.print(": ");
+//      Serial.println(sample[k], BIN);
+      Serial.println(((double)sample[k]/4096.0)*5.0);
+      delay(5000);
     }
     interrupts();
     i = 0;
