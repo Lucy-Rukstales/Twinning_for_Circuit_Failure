f6d7fb86da833ed8a55f15c2d93bf783c8b71590
Lucy Rukstales <43323790+Lucy-Rukstales@users.noreply.github.com>
2020-08-05 20:58:36 -0400

    Update Fast_ADC_Read_12bit.v

--- Fast_ADC_Read_12bit.v
+++ Fast_ADC_Read_12bit.v
@@ -7,7 +7,7 @@
 // Top Module:		Data_Collector.v
 //
 // Description: 	This file allows for 12 bits of data collection from an analog to digital converter (ADC)
-//			This runs the ADC using the 12.5MHz FPGA clock; 12-bit samples sent at roughly 781.25kHz
+//			This runs the ADC using the 25MHz FPGA clock; 12-bit samples sent at roughly 1.5MHz
 //
 //////////////////////////////////////////////////////
 
@@ -18,31 +18,31 @@ module Fast_ADC_Read_12bit(clk,rst,CS,SCK,SDO,sample,cnt15);
 	input rst; // Reset switch
 	
 	output reg CS; // Chip Select - Turns ADC on
-	output reg SCK; // ADC - 12.5MHz clock
+	output reg SCK; // ADC - 25MHz clock
 	
 	output reg [11:0]sample; // 12-bit data sample
 	output reg [4:0]cnt15;   // Counter to step through ADC control
 	
-	reg [2:0]counter; // Counter to establish 12.5MHz ADC clock
+	reg [1:0]counter; // Counter to establish 25MHz ADC clock
 		
 	//----------------------------------------------------
-	// Create a counter to divide 50MHz FPGA clock to 12.5MHz ADC clock
+	// Create a counter to divide 50MHz FPGA clock to 25MHz ADC clock
 	always @ (posedge clk or negedge rst) begin
 		
-		if (rst == 1'b0) counter <= 3'd0;
+		if (rst == 1'b0) counter <= 2'd0;
 		
 		else begin
 		
-			if (counter < 3'd3) counter <= counter + 3'd1;
+			if (counter == 2'd0) counter <= 2'd1;
 			
-			else counter <= 3'd0;
+			else counter <= 2'd0;
 			
 		end
 			
 	end
 	
 	//----------------------------------------------------
-	// Scale clock from 50MHz to 12.5MHz
+	// Scale clock from 50MHz to 25MHz
 	// SCK to be ADC clock
 	always @ (posedge clk or negedge rst) begin
 		
@@ -50,9 +50,9 @@ module Fast_ADC_Read_12bit(clk,rst,CS,SCK,SDO,sample,cnt15);
 		
 		else begin
 		
-			if (counter == 3'd0) SCK <= 1'b0;
+			if (counter == 2'd0) SCK <= 1'b0;
 			
-			else if (counter == 3'd2) SCK <= 1'b1;
+			else if (counter == 2'd1) SCK <= 1'b1;
 		
 		end
 		
@@ -64,7 +64,7 @@ module Fast_ADC_Read_12bit(clk,rst,CS,SCK,SDO,sample,cnt15);
 		
 		if (rst == 1'b0) cnt15 <= 5'd0;
 		
-		else if (counter == 3'd0 && cnt15 <= 5'd16) cnt15 <= cnt15 + 5'd1;
+		else if (counter == 2'd0 && cnt15 <= 5'd16) cnt15 <= cnt15 + 5'd1;
 		
 		else cnt15 <= cnt15;
 		
@@ -82,9 +82,9 @@ module Fast_ADC_Read_12bit(clk,rst,CS,SCK,SDO,sample,cnt15);
 		
 		end
 		
-		else if (counter == 3'd2 && (cnt15 == 5'd0 || cnt15 == 5'd15)) CS <= 1'b0;
+		else if (counter == 2'd1 && (cnt15 == 5'd0 || cnt15 == 5'd15)) CS <= 1'b0;
 		
-		else if (counter == 3'd0) begin
+		else if (counter == 2'd0) begin
 			
 			if (counter == 3'd1 && cnt15 > 5'd1 && cnt15 < 5'd14) sample[11:0] <= {sample[10:0],SDO};
 		
