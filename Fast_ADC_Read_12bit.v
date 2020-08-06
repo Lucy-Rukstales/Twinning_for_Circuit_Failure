c82bda3c99ad984b20c9cd5d8356c39d2a56a258
Lucy Rukstales <43323790+Lucy-Rukstales@users.noreply.github.com>
2020-08-05 21:09:06 -0400

    Update Fast_ADC_Read_12bit.v

--- Fast_ADC_Read_12bit.v
+++ Fast_ADC_Read_12bit.v
@@ -21,50 +21,32 @@ module Fast_ADC_Read_12bit(clk,rst,CS,SCK,SDO,sample,cnt15);
 	output reg SCK; // ADC - 25MHz clock
 	
 	output reg [11:0]sample; // 12-bit data sample
-	output reg [4:0]cnt15;   // Counter to step through ADC control
-	
-	reg [1:0]counter; // Counter to establish 25MHz ADC clock
-		
-	//----------------------------------------------------
-	// Create a counter to divide 50MHz FPGA clock to 25MHz ADC clock
-	always @ (posedge clk or negedge rst) begin
-		
-		if (rst == 1'b0) counter <= 2'd0;
-		
-		else begin
-		
-			if (counter == 2'd0) counter <= 2'd1;
-			
-			else counter <= 2'd0;
-			
-		end
-			
-	end
+	output reg [4:0]cnt15;   // SCK to step through ADC control
 	
 	//----------------------------------------------------
 	// Scale clock from 50MHz to 25MHz
 	// SCK to be ADC clock
 	always @ (posedge clk or negedge rst) begin
 		
-		if (rst == 1'b0) SCK <= 1'b0;
+		if (rst == 1'b0) SCK <= 2'd0;
 		
 		else begin
 		
-			if (counter == 2'd0) SCK <= 1'b0;
+			if (SCK == 2'd0) SCK <= 2'd1;
 			
-			else if (counter == 2'd1) SCK <= 1'b1;
-		
+			else SCK <= 2'd0;
+			
 		end
-		
+			
 	end
-		
+	
 	//----------------------------------------------------
 	// Count to 15 to step through ADC initialization and data transfer
 	always @ (posedge clk or negedge rst) begin
 		
 		if (rst == 1'b0) cnt15 <= 5'd0;
 		
-		else if (counter == 2'd0 && cnt15 <= 5'd16) cnt15 <= cnt15 + 5'd1;
+		else if (SCK == 2'd0 && cnt15 <= 5'd16) cnt15 <= cnt15 + 5'd1;
 		
 		else cnt15 <= cnt15;
 		
@@ -82,11 +64,11 @@ module Fast_ADC_Read_12bit(clk,rst,CS,SCK,SDO,sample,cnt15);
 		
 		end
 		
-		else if (counter == 2'd1 && (cnt15 == 5'd0 || cnt15 == 5'd15)) CS <= 1'b0;
+		else if (SCK == 2'd1 && (cnt15 == 5'd0 || cnt15 == 5'd15)) CS <= 1'b0;
 		
-		else if (counter == 2'd0) begin
+		else if (SCK == 2'd0) begin
 			
-			if (counter == 3'd1 && cnt15 > 5'd1 && cnt15 < 5'd14) sample[11:0] <= {sample[10:0],SDO};
+			if (SCK == 3'd1 && cnt15 > 5'd1 && cnt15 < 5'd14) sample[11:0] <= {sample[10:0],SDO};
 		
 			else sample <= sample;
 			
